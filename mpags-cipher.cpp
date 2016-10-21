45e8bf88ea53984b42557c33ef3ca5cac46a8afe
phrfbr <sammyvalder@gmail.com>
2016-10-21 16:42:09 +0100

    updated mpags cipher to end of week 1

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,11 +1,67 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
-#include <ctype.h>
+#include <cctype>
+#include <vector>
 
-int main()
+int main(int argc, char* argv[])
 {
 
+//Convert command line arguments 
+std::vector<std::string> cmdLineArgs { argv, argv+argc };
+
+// Print the command line arguments
+int i = cmdLineArgs.size();
+for (int j =0; j<i ; j++ )
+{	
+	//If help is asked for, give it
+	if (cmdLineArgs[j] == "-h" || cmdLineArgs[j] == "--help")
+	{
+		std::string help {"This is help\n"};
+		std::cout << help << std::endl;
+		return 0;
+	}
+
+	else if (cmdLineArgs[j] == "--version")
+	{
+		std::string version {"Version 1.0"};
+		std::cout << version << std::endl;
+		return 0;
+	}	
+
+	else if (cmdLineArgs[j] == "-i")
+	{	
+
+		if (j+1 == i)
+		{	
+			std::cout << "ERROR: no input file defined" << std::endl;
+			return 0;
+		}
+
+		std::string inputfile {""};
+		inputfile += cmdLineArgs[j+1];
+		std::cout << "Input file saves as variable inputfile" << std::endl;
+
+	}
+
+	else if (cmdLineArgs[j] == "-o")
+	{
+
+		if (j+1 == i)
+		{	
+			std::cout << "ERROR: no output file defined" << std::endl;
+			return 0;
+		}
+
+
+		std::string outputfile {""};
+		outputfile += cmdLineArgs[j+1];
+		std::cout << "Output file saves as variable outputfile" << std::endl;
+
+	}
+
+
+	std::cout << cmdLineArgs[j] << std::endl;
+}
 std::string msg {""};
 
 // Take each letter from user input and in each case:
@@ -18,8 +74,9 @@ std::string msg {""};
 		//    - Convert to upper case	
 		if(isalpha(in_char))
 		{
-			msg += in_char;	
 			in_char = toupper(in_char);
+			msg += in_char;	
+
 		}
 
 		//    - Ignore any other (non-alpha) characters
@@ -67,7 +124,6 @@ std::string msg {""};
 	}
 
 	std::cout << msg << std::endl;
-
 
 //    - In each case, add result to a string variable
 // print out the new string	
